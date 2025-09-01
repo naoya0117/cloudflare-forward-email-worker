eaad5f58416aa87273a73582a6187c74574d6691
Naoya Matsuhashi <mmn117302@outlook.jp>
2025-09-02 00:31:44 +0900

    refactor

--- src/handler/emailHandler/index.ts
+++ src/handler/emailHandler/index.ts
@@ -1,7 +1,8 @@
 import { Environment } from  "../../type.d";
+import { validateEnv } from "../../utils/validatedEnv";
 
 /**
- * メールをFORWARD_EMAILに転送する関数
+ * メールをFORWARD_EMAILに転送し、エラーが起きたらFALLBACK_EMAILに転送する
  * @param message 
  * @param env 
  * @param _ctx 
@@ -9,14 +10,29 @@ import { Environment } from  "../../type.d";
  */
 export const emailHandler = async(message: ForwardableEmailMessage, env: Environment, _ctx: ExecutionContext): Promise<Response> => {
 
-    const { FORWARD_EMAIL, FALLBACK_EMAIL } = env;
+    const { FORWARD_EMAIL, FALLBACK_EMAIL } = validateEnv(env);
 
     try {
-        await message.forward(FORWARD_EMAIL);
+        await forwardEmail(message, FORWARD_EMAIL);
     } catch (error) {
+        // エラーが起きたらフォールバック用のメールアドレスに転送する
         console.warn("Warning: Primary forward failed:", (error as Error).message);
-        await message.forward(FALLBACK_EMAIL);
+        await forwardEmail(message, FALLBACK_EMAIL);
     }
 
     return new Response("Email forwarded successfully", { status: 200 });
+}
+
+/**
+ * メールを転送する関数
+ * @param message 
+ * @param email 
+ */
+const forwardEmail = async(message: ForwardableEmailMessage, email: string) => {
+    try {
+        await message.forward(email);
+    } catch (error) {
+        console.warn("Warning: Forward failed:", (error as Error).message);
+        throw error;
+    }
 }
---
+++ src/utils/validatedEnv.ts
@@ -0,0 +1,25 @@
+import { Environment } from "../type.d"
+
+/**
+ * バリデーション済み環境変数の型定義
+ * すべてのフィールドが必須かつnon-nullableとなる
+ */
+type ValidatedEnv = {
+    [key in keyof Environment]-?: NonNullable<Environment[key]>;
+}
+
+/**
+ * 環境変数をバリデーションする
+ * @param env 環境変数
+ * @returns バリデーション済み環境変数
+ */
+export const validateEnv = (env: Environment): ValidatedEnv => {
+    for (const [key, value] of Object.entries(env)) {
+        if (!value) {
+            throw new Error(`${key} is not set`)
+        }
+
+    }
+
+    return env as ValidatedEnv;
+}
